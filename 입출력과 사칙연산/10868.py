37d57d07b847780d9579d28da111355b03ad870b
FourKown <dydqndaos123@naver.com>
2023-10-12 22:36:09 +0900

    first

---
+++ 입출력과 사칙연산/10868.py
@@ -0,0 +1,7 @@
+number1, number2 = map(int, input().split())
+
+print(number1 + number2) # 더하기
+print(number1 - number2) # 빼기
+print(number1 * number2) # 곱하기
+print(number1 // number2) # 나누기 (몫)
+print(number1 % number2) # 나누기 (나머지)
